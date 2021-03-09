9286a6160a0d4fe1a32bec0d496ac770cbefc1fa
AdeptusM <39475955+AdeptusM@users.noreply.github.com>
2021-03-08 19:05:19 -0500

    Add files via upload

---
+++ 419.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head>
+  
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+  <title>419 Scams - Would You Believe It?</title>
+
+  
+</head><body>
+<
+<br>
+
+<span style="font-family: Georgia; font-weight: bold;">419 SCAMS -
+WOULD YOU BELIEVE IT?</span><br style="font-family: Georgia;">
+
+<br style="font-family: Georgia;">
+
+<span style="font-family: Georgia;">Let me paint the scene, you are
+sitting in your uncomfortable chair in front of your too-slow
+workstation, lounging in your o-so-big cubicle. Suddenly, you receive
+an email, this email offers you riches beyond your wildest dreams of
+avarice if you just trust the sender and send them some details. Guess
+what? You have just gotten a 419 scam email, cue the cheesy drum roll
+sound effect. Lets look at this in more detail...</span><br style="font-family: Georgia;">
+
+<br style="font-family: Georgia;">
+
+<span style="font-family: Georgia; font-style: italic;">What is it?</span><br style="font-family: Georgia;">
+
+<span style="font-family: Georgia;">Well, the sender is a criminal who
+wants to empty your bank account, to do this he needs some details
+regarding the account. They get this by offering their victims lots of
+money, letting greed work it's magic. Once they have the details they
+empty the account. Thats about it. The details differ on the stories
+used and how believable they are but the aim and the end result remain
+the same. Take a look at this example 419 email..</span><br>
+
+<table style="width: 80%; height: 447px; text-align: left; margin-left: auto; margin-right: auto;" border="1" cellpadding="2" cellspacing="2">
+
+  <tbody>
+    <tr>
+      <td>
+      <p><samp>Dear Sir/Madam </samp></p>
+      <p><samp>I am the eldest son of the late Henri Obi Kabongo (a
+foremost human rights activist in the Democratic Republic of Congo). My
+father died in March 2000 as a result of the inhumane treatment meted
+out on him while in prison by the government of Laurent Kabila.
+However, it is particularly important to mention here that before the
+death of my father, he was a successful businessman and his business
+empire includes real estate, exportation, and consultancy services
+which cut across Africa. He was able to accumulate enough money both in
+local and foreign currencies through his business empire. In fact, he
+was at one time rated among the first five richest Congolese
+businessmen by one of the leading business magazines in my country. His
+entire business has been on the upward trend over the past ten years,
+bringing his net profit during the period under review to Twenty one
+million U.S dollars ($21,000,000). This of course does not include his
+assets which are already running into millions of dollars. Having said
+all this, there was a twist in my father&#8217;s success story
+following his confrontation with the government of Laurent Kabila who
+has been quite high-handed and dictatorial in his policies and
+day-to-day running of the affairs of the Congolese people. My father
+being a human rights activist, criticised his government in both local
+and international newspapers. This infuriated him and he clamped my
+father in jail until he died in March last year due to ill health.
+Fortunately, before the arrest of my father, his attorney acting upon
+his instructions, transferred the sum of Twenty one million U.S dollars
+($21,000,000) from his local banks to a security firm overseas. He did
+this for fear of this accounts being frozen by the government.
+Following my father&#8217;s death, my entire family decided to seek
+political assylum in Lome-Togo, as the eldest son of my late father and
+according to our tradition in Africa. I have now inherited all my
+father&#8217;s wealth. Considering the present face-off between Kabila
+and my family, I was then advised by my father&#8217;s attorney
+(presently my attorney) to seek the assistance of someone based in your
+country to receive the amount in question being ($21,000,000). Since I
+have the intention of relocating my family there with the aim of
+settling down there and investing. I have decided to give you 5% of the
+total amount involved and I have also set aside 5% for any expenses
+that might be incurred in the course of this transaction. To finalize
+this transaction you will have to contact me via my e-mail address
+(xxxxxi@mail.com). further discussions will be centered on how and when
+we shall be meet after the fund is in your account. I shall expect you
+to keep this transaction confidential due to the present circumstance
+of my family and I urgently await your response.<br>
+Yours faithfully <br>
+Peter Obi</samp> </p>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<br>
+
+<span style="font-family: Georgia;">Don't you you just feel the urge to
+contact this poor soul and help him out? If you do please contact me as
+as well, I've got this bridge in Brooklyn I am trying to sell, I'll
+even throw in this old tower I have in Paris....</span><br style="font-family: Georgia;">
+
+<br style="font-family: Georgia;">
+
+<span style="font-family: Georgia; font-style: italic;">Does It
+Actually Work?</span><br style="font-family: Georgia;">
+
+<span style="font-family: Georgia;">Do people actually fall for this
+scam? In a word ... YES. It works because of one inescapable fact of
+human nature - Greed. And if you think this is a new thing, think
+again. The earliest record of this type of scam was from the 1920's,
+when it was known as the Spanish Prisoner scam. People were asked to
+help secure the release from prison of &nbsp;the son of a wealthy
+Spanish family, who would -of course- be very financially grateful. But
+the result then was the same as now, the victims ended up out of
+pocket. The modern day problem has gotten so bad, that in the US the
+estimated loss is in the Millions of dollars. In fact, the secret
+service have a web page dedicated to it: </span><a style="font-family: Georgia;" href="http://www.secretservice.gov/alert419.shtml">http://www.secretservice.gov/alert419.shtml.</a><span style="font-family: Georgia;"> Or take a look at the </span><a style="font-family: Georgia;" href="http://home.rica.net/alphae/419coal/">419 coalition website</a><span style="font-family: Georgia;">.</span><br style="font-family: Georgia;">
+
+<br style="font-family: Georgia;">
+
+<span style="font-family: Georgia; font-style: italic;">Do They Know
+Who I Am?</span><br style="font-family: Georgia;">
+
+<span style="font-family: Georgia;">While the email ends up in your
+inbox, that does not mean the sender knows of you personally, they just
+want you to feel that way. Rather, the email finds you in the same way
+that normal, everyday spam (damn, that sounds strange) does. People
+selling email lists, buying email lists, trawling the web, the victim
+making their email address known, or any other spammer trick. The 419
+sent out hundreds of these emails looking for that one greedy sucker
+(one born every minute).</span><br style="font-family: Georgia;">
+
+<br style="font-family: Georgia;">
+
+<span style="font-family: Georgia; font-style: italic;">What Can I Do?</span><br style="font-family: Georgia;">
+
+<span style="font-family: Georgia;">Well, you can use any of the normal
+anti-spam techniques but a simple solution is to follow a simple rule: <span style="font-style: italic;">If it seems to good to be true, thats
+because it is!</span> Do not be greedy and you won't get caught. If you
+are one of those crusading types, take a look at an </span><a style="font-family: Georgia;" href="http://www.419eater.com/">anti-419
+scam</a><span style="font-family: Georgia;"> website, it is good for a
+laugh.</span><br style="font-family: Georgia;">
+
+<br>
+</body></html>
